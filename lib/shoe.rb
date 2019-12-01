99f464a569f20fd6a348c8c1dcf2734b85fa3e42
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-01 01:05:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,12 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material,
   
-  def initialize(condition)
+  def initialize(condition = "tattered")
+    @condition = condition
+  end
+  
+  def condition
     @condition
+  end
   
 end
